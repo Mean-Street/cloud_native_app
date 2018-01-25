e239a779dffa10f63c2c4914c05edcd433685d0d
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-01-25 18:06:17 +0100

    Delete test instance after testing

--- deployment/openstack/deploy_test.sh
+++ deployment/openstack/deploy_test.sh
@@ -2,8 +2,9 @@
 
 . ./config.sh
 
-cmd "openstack server delete --wait $TEST_NAME"
 cmd "openstack server create --image $IMAGE_ID --flavor $FLAVOR_ID --key-name $KEY_NAME --network $PRIV_NETWORK --wait $TEST_NAME"
 cmd "openstack server add floating ip $TEST_NAME $TEST_FLOATING_IP"
 
 . ./deploy_app.sh $TEST_FLOATING_IP $PRIV_KEY_PATH
+
+cmd "openstack server delete $TEST_NAME"
