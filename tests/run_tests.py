932c77649b675d47712e5c657ef8b878528106bb
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2018-01-28 19:44:17 +0100

    Wrote tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,33 +1,42 @@
 #! /usr/bin/python3
 
+import sys
 import requests
+import random
+
+RED = '\033[31m'
+GREEN = '\033[32m'
+END_COLOR = '\033[0m'
+success = GREEN + "PASSED" + END_COLOR
+failure = RED + "FAILED" + END_COLOR
+
 
 host = "http://localhost:"
+services = {
+"i":8080,
+"s":8081,
+"b":8082,
+"p":8083,
+"w":8090
+}
 
-print("###### Test: rabbitMQ")
-#  r = requests.get(host + "15672" + "/")
 
-print("###### Test: redis")
-#  6379
+error = 0
+for service, port in services.items():
+    print("[TEST][SERVICE " + service + "]: ", end="")
+    try:
+        response = request.get(host + port + "/").json()
+        assert(response["Service"] == "Microservice " + service)
+        print(success)
+    except:
+        print(failure)
+        error = 1
 
-print("###### Test: db")
-#  3306 
+sys.exit(error)
 
-print("###### Test: web")
-#  80
-
-print("###### Test: i")
-#  8080
-
-print("###### Test: s")
-#  8081
-
-print("###### Test: b")
-#  8082
-
-print("###### Test: p")
-#  8083
-
-print("###### Test: w")
-#  8090
+# TODO:
+# rabbitMQ 15672
+# redis 6379
+# db 3306 
+# web 80
 
