ed54831d6437b7d9b435e4dc2cfdc19bafa28cc9
Nogueira Vitor <vitor.ribeiro-nogueira@ensimag.fr>
2018-01-28 15:56:02 +0100

    Add of upgrade command to the instance, so the sources.list is updated

--- deployment/openstack/deploy_app.sh
+++ deployment/openstack/deploy_app.sh
@@ -11,6 +11,7 @@ cmd_instance "sudo sh -c 'echo LANG=en_US.utf-8 > /etc/environment'" $IP
 cmd_instance "sudo sh -c 'echo LC_ALL=en_US.utf-8 >> /etc/environment'" $IP
 
 cmd_instance "sudo apt-get -y update" $IP
+cmd_instance "sudo apt-get -y upgrade" $IP
 cmd_instance "sudo apt-get -y install git docker.io python3-pip" $IP
 cmd_instance "sudo pip3 install --upgrade pip" $IP
 cmd_instance "sudo pip3 install docker-compose" $IP
