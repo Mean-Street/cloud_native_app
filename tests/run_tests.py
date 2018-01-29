d8fe85cdad9d1947b304aeddd11e800d033671af
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-01-29 12:29:02 +0100

    Fix script running tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,37 +2,34 @@
 
 import sys
 import requests
-import random
 
-RED = '\033[31m'
-GREEN = '\033[32m'
-END_COLOR = '\033[0m'
-success = GREEN + "PASSED" + END_COLOR
-failure = RED + "FAILED" + END_COLOR
+SUCCESS_MSG = "PASSED"
+FAILURE_MSG = "FAILED"
 
-
-host = "http://localhost:"
-services = {
-"i":8080,
-"s":8081,
-"b":8082,
-"p":8083,
-"w":8090
+HOST = "http://localhost:"
+PORTS = {
+    "i": 8080,
+    "s": 8081,
+    "b": 8082,
+    "p": 8083,
+    "w": 8090
 }
 
+if __name__ == "__main__":
+    error = 0
+    for service, port in PORTS.items():
+        print("[TEST][SERVICE " + service + "]: ", end="")
+        try:
+            r = requests.get(HOST + str(port) + "/")
+            assert r.status_code // 200 == 1
+            assert r.json()["Service"] == "Microservice " + service
+            print(SUCCESS_MSG)
+        except Exception as e:
+            print(FAILURE_MSG)
+            print(e)
+            error = 1
 
-error = 0
-for service, port in services.items():
-    print("[TEST][SERVICE " + service + "]: ", end="")
-    try:
-        response = request.get(host + port + "/").json()
-        assert(response["Service"] == "Microservice " + service)
-        print(success)
-    except:
-        print(failure)
-        error = 1
-
-sys.exit(error)
+    sys.exit(error)
 
 # TODO:
 # rabbitMQ 15672
