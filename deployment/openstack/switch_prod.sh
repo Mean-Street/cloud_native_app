b4ac47ac8e9c9ecb9cb8704216edcd1c4b38f9cf
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-01-29 16:56:04 +0100

    Fix production instance switching

--- deployment/openstack/switch_prod.sh
+++ deployment/openstack/switch_prod.sh
@@ -5,7 +5,6 @@ set -e
 . ./tools.sh
 
 cmd "openstack server remove floating ip $PROD_NAME $PROD_TMP_FLOATING_IP"
-cmd "openstack server remove floating ip $PROD_OLD_NAME $PROD_FLOATING_IP"
 cmd "openstack server add floating ip $PROD_NAME $PROD_FLOATING_IP"
 
 cmd "openstack server delete --wait $PROD_OLD_NAME"
