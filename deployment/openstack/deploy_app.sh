bc4e22f4c1a5d9c4b4449c0abf836fa51bc87665
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-01-29 10:32:35 +0100

    Do not exit on first failure in app deployment

--- deployment/openstack/deploy_app.sh
+++ deployment/openstack/deploy_app.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-set -e
-
 IP=$1
 
 . ./tools.sh
 
-cmd_instance "sudo sh -c 'echo nameserver 8.8.8.8 > /etc/resolv.conf'" $IP
-cmd_instance "sudo sh -c 'echo LANG=en_US.utf-8 > /etc/environment'" $IP
-cmd_instance "sudo sh -c 'echo LC_ALL=en_US.utf-8 >> /etc/environment'" $IP
+./init_instance.sh
+if [$? -ne 0]
+then
+    exit 1
+fi
 
-cmd_instance "sudo apt-get -y update" $IP
-cmd_instance "sudo apt-get -y upgrade" $IP
-cmd_instance "sudo apt-get -y install git docker.io python3-pip" $IP
-cmd_instance "sudo pip3 install --upgrade pip" $IP
-cmd_instance "sudo pip3 install docker-compose" $IP
-
-cmd_instance "sudo systemctl start docker" $IP
-cmd_instance "git clone https://github.com/Mean-Street/cloud_native_app" $IP
+cmd_instance "cd cloud_native_app; git pull" $IP
 
 i=0
 err=1
