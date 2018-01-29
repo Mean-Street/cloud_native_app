dd8a7f02d84090f45a2fb73c2828b053b93b5fbf
Vincent Lefoulon <vincent.lefoulon@free.fr>
2018-01-29 22:07:12 +0100

    Do not exit when prod instance does not exist

--- deployment/openstack/deploy_prod.sh
+++ deployment/openstack/deploy_prod.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-set -e
-
 . ./tools.sh
 
 cmd "openstack server set --name $PROD_OLD_NAME $PROD_NAME"
+
+set -e
+
 cmd "openstack server create --image $IMAGE_ID --flavor $FLAVOR_ID --security-group $GLOBAL_SECURITY_GROUP --security-group $PROD_SECURITY_GROUP --key-name $KEY_NAME --network $PRIV_NETWORK --wait $PROD_NAME"
 cmd "openstack server add floating ip $PROD_NAME $PROD_TMP_FLOATING_IP"
 
